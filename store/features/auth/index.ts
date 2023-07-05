64306b285444cf16c7d0bf6f60a41582d164d2c2
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2023-07-05 23:23:44 +0100

    redux store.

--- store/features/auth/index.ts
+++ store/features/auth/index.ts
@@ -1,41 +1,53 @@
-import { PayloadAction, createSlice } from '@reduxjs/toolkit';
-import { AppState } from '../../index';
-import { HYDRATE } from 'next-redux-wrapper';
+import { PayloadAction, createSlice } from '@reduxjs/toolkit'
+import { AppState } from '../../index'
+import { HYDRATE } from 'next-redux-wrapper'
+import { User } from '@firebase/auth'
 
 // Type for our state
 export interface AuthState {
-  authState: boolean;
+    loading: boolean
+    user: User | null
 }
 
 // Initial state
 const initialState: AuthState = {
-  authState: false,
-};
+    loading: false,
+    user: null,
+}
 
 // Actual Slice
 export const authSlice = createSlice({
-  name: 'auth',
-  initialState,
-  reducers: {
-    // Action to set the authentication status
-    setAuthState(state, action: PayloadAction<boolean>) {
-      state.authState = action.payload;
+    name: 'auth',
+    initialState,
+    reducers: {
+        // Action to set the authentication status
+        setLoading(state, action: PayloadAction<{ loading: boolean }>) {
+            return {
+                ...state,
+                loading: action.payload.loading,
+            }
+        },
+        setUser(state, action: PayloadAction<User | null>) {
+            return {
+                loading: false,
+                user: action.payload,
+            }
+        },
     },
-  },
 
-  // Special reducer for hydrating the state. Special case for next-redux-wrapper
-  extraReducers: (builder) => {
-    builder.addCase(HYDRATE, (state, action: any) => {
-      return {
-        ...state,
-        ...action.payload.auth,
-      };
-    });
-  },
-});
+    // Special reducer for hydrating the state. Special case for next-redux-wrapper
+    extraReducers: (builder) => {
+        builder.addCase(HYDRATE, (state, action: any) => {
+            return {
+                ...state,
+                ...action.payload.auth,
+            }
+        })
+    },
+})
 
-export const { setAuthState } = authSlice.actions;
+export const { setLoading, setUser } = authSlice.actions
 
-export const selectAuthState = (state: AppState) => state.auth.authState;
+export const selectAuthState = (state: AppState) => state.auth.authState
 
-export default authSlice.reducer;
+export default authSlice.reducer
