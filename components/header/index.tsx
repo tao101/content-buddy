acba83a08c031bebe2fd4ecec89814bcd877eb0f
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2023-07-05 23:22:03 +0100

    added menu.

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,9 +1,13 @@
 import Link from 'next/link'
-import { useRouter } from 'next/router'
 import { toast } from 'react-toastify'
+import { useState } from 'react'
 
 export default function Header() {
-    const router = useRouter()
+    const [menuOpen, setMenuOpen] = useState(false)
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen)
+    }
 
     // const onSettingsClick = () => {
     //   router.push('/settings');
@@ -15,13 +19,59 @@ export default function Header() {
 
     return (
         <div className="font-sans w-full text-white flex items-center px-12 py-6">
-            <div className="border border-gray-400 rounded cursor-pointer">
+            <div
+                className="border border-gray-400 rounded cursor-pointer"
+                onClick={toggleMenu}
+            >
                 <img
                     src="/menu.png"
                     width={18}
                     height={20}
                     className="my-1 mx-2"
+                    alt="Menu"
                 />
+            </div>
+
+            <div
+                className={`menu-overlay ${menuOpen ? 'open' : ''} `}
+                onClick={toggleMenu}
+            >
+                <div
+                    className={`menu-content ${
+                        menuOpen ? 'open' : ''
+                    } bg-primary`}
+                >
+                    <button className="text-white text-2xl mb-4 hover:underline hover:underline-offset-1 ">
+                        Home
+                    </button>
+                    <button className="text-white text-2xl mb-4 hover:underline hover:underline-offset-1 ">
+                        About Us
+                    </button>
+                    <button className="text-white text-2xl mb-4 hover:underline hover:underline-offset-1 ">
+                        FAQ
+                    </button>
+                    <button className="text-white text-2xl mb-4 hover:underline hover:underline-offset-1 ">
+                        Pricing
+                    </button>
+                    <div
+                        className="close-button cursor-pointer"
+                        onClick={toggleMenu}
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24"
+                            width="30"
+                            height="30"
+                            fill="none"
+                            stroke="currentColor"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                        >
+                            <path d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+                    </div>
+                </div>
             </div>
         </div>
     )
