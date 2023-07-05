cfa4ad926839e94e14747727cfe29ce4aff9080e
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2023-07-05 23:21:18 +0100

    signup config.

--- components/trial/trial.tsx
+++ components/trial/trial.tsx
@@ -1,4 +1,14 @@
+import { useRouter } from 'next/router'
+
 export default function Trial() {
+    const router = useRouter()
+    // const onSettingsClick = () => {
+    //   router.push('/settings');
+    // };
+
+    const onSignupClick = () => {
+        router.push('/signup')
+    }
     return (
         <div className="flex flex-col items-center content-center text-center md:max-w-[70%]  mx-auto px-12 ">
             <h1 className="capitalize text-3xl font-bold">content Buddy</h1>
@@ -7,7 +17,10 @@ export default function Trial() {
                 tincidunt mauris eget dui dictum, a tristique ante elementum.
                 Donec ac risus vitae quam ornare euismod eu a urna.
             </h1>
-            <div className="rounded-full bg-secondary px-8 py-2 mt-4 cursor-pointer">
+            <div
+                className="rounded-full bg-secondary px-8 py-2 mt-4 cursor-pointer"
+                onClick={onSignupClick}
+            >
                 Try it for free
             </div>
         </div>
--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,9 +1,25 @@
-import Image from 'next/image'
+import { auth } from '../services/firebase'
+import { signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
-const Signup = () => {
+export default function Signup() {
+    const router = useRouter()
+
+    const onSingupWithGoogle = async () => {
+        const googleAuth = new GoogleAuthProvider()
+        try {
+            await signInWithPopup(auth, googleAuth)
+            router.push('/welcome')
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     return (
         <div className="flex flex-row h-screen ">
-            <div className="left-column bg-gray-800 ">
+            <div className="flex-1 flex flex-col justify-center items-center bg-gray-800 ">
                 <img src="/brain.svg" width={120} height={120} />
                 <div className="text-[24px] font-bold text-white">
                     Create Account
@@ -15,7 +31,10 @@ const Signup = () => {
                         Sign in
                     </span>
                 </div>
-                <button className="flex items-center justify-center p-[10px] w-[350px] bg-secondary text-white text-md font-[500] rounded-[5px] cursor-pointer mb-[10px]">
+                <button
+                    onClick={onSingupWithGoogle}
+                    className="flex items-center justify-center p-[10px] w-[350px] bg-secondary text-white text-md font-[500] rounded-[5px] cursor-pointer mb-[10px]"
+                >
                     <img
                         src="/google-icon.png"
                         width={20}
@@ -24,7 +43,7 @@ const Signup = () => {
                     />
                     Continue with Google
                 </button>
-                <div className="divider">or</div>
+                <div className="my-4 text-gray-400">or</div>
 
                 <input
                     type="email"
@@ -35,7 +54,7 @@ const Signup = () => {
                     Continue with email
                 </button>
             </div>
-            <div className="right-column bg-gray-700 ">
+            <div className="flex-2 flex items-center justify-center right-column bg-gray-700 ">
                 <img
                     src="/Group1.svg"
                     width={800}
@@ -44,67 +63,6 @@ const Signup = () => {
                     className="mx-auto cursor-pointer"
                 />
             </div>
-            <style jsx>{`
-                .left-column {
-                    flex: 1;
-                    display: flex;
-                    flex-direction: column;
-                    justify-content: center;
-                    align-items: center;
-                }
-
-                .right-column {
-                    flex: 2;
-                    display: flex;
-
-                    align-items: center;
-                    justify-content: center;
-                }
-
-                .subtitle {
-                    font-size: 18px;
-                    margin-bottom: 10px;
-                }
-
-                .button {
-                    display: flex;
-                    align-items: center;
-                    justify-content: center;
-                    padding: 10px 20px;
-                    background-color: #4285f4;
-                    color: #fff;
-                    font-size: 16px;
-                    font-weight: bold;
-                    border: none;
-                    border-radius: 5px;
-                    cursor: pointer;
-                    margin-bottom: 10px;
-                }
-
-                .divider {
-                    display: flex;
-                    align-items: center;
-                    justify-content: center;
-                    margin: 20px 0;
-                    color: #999;
-                    font-size: 16px;
-                }
-                .continue-button {
-                    display: flex;
-                    align-items: center;
-                    justify-content: center;
-                    padding: 10px 20px;
-                    background-color: #4285f4;
-                    color: #fff;
-                    font-size: 16px;
-                    font-weight: bold;
-                    border: none;
-                    border-radius: 5px;
-                    cursor: pointer;
-                }
-            `}</style>
         </div>
     )
 }
-
-export default Signup
