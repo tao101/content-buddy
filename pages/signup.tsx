c068c69e218337c86e434aaa18de716300e5102b
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2023-07-10 20:38:07 +0100

    mediaQueries for SignIn.

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -43,67 +43,65 @@ export default function Signup() {
     }
 
     return (
-        <>
-            <div className="flex flex-row h-screen ">
-                <div className="flex-1 flex flex-col justify-center items-center bg-gray-800 ">
-                    <img src="/brain.svg" width={120} height={120} />
-                    <div className="text-[24px] font-bold text-white">
-                        Create Account
-                    </div>
-                    <div className="text-gray-300 mb-4">
-                        Already have an account?{' '}
-                        <span
-                            onClick={onSignInClick}
-                            className="text-secondary font-bold text-lg cursor-pointer"
-                        >
-                            {' '}
-                            Sign in
-                        </span>
-                    </div>
-                    <button
-                        onClick={onSignupWithGoogle}
-                        className="flex items-center justify-center p-[10px] w-[350px] bg-secondary text-white text-md font-[500] rounded-[5px] cursor-pointer mb-[10px]"
+        <div className="flex flex-row h-screen  ">
+            <div className=" max-md:w-full flex flex-col justify-center items-center bg-gray-800 w-fit lg:px-16 md:px-8">
+                <img src="/brain.svg" width={120} height={120} />
+                <div className="text-[24px] font-bold text-white">
+                    Create Account
+                </div>
+                <div className="text-gray-300 mb-4">
+                    Already have an account?{' '}
+                    <span
+                        onClick={onSignInClick}
+                        className="text-secondary font-bold text-lg cursor-pointer"
                     >
-                        <BiLogoGoogle className="my-auto mx-1 text-center w-[20px] h-[20px]" />
-                        Continue with Google
-                    </button>
-                    <div className="my-4 text-gray-400">or</div>
+                        {' '}
+                        Sign in
+                    </span>
+                </div>
+                <button
+                    onClick={onSignupWithGoogle}
+                    className="flex items-center justify-center p-[10px] w-[350px] bg-secondary text-white text-md font-[500] rounded-[5px] cursor-pointer mb-[10px]"
+                >
+                    <BiLogoGoogle className="my-auto mx-1 text-center w-[20px] h-[20px]" />
+                    Continue with Google
+                </button>
+                <div className="my-4 text-gray-400">or</div>
 
+                <input
+                    type="email"
+                    className="px-4 py-[10px] order-solid bg-gray-500  rounded-[15px] mb-[10px] w-[350px] text-white focus:outline-0"
+                    onClick={() => setOnShowPassword(!onShowPassword)}
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                {onShowPassword && (
                     <input
-                        type="email"
+                        type="password"
                         className="px-4 py-[10px] order-solid bg-gray-500  rounded-[15px] mb-[10px] w-[350px] text-white focus:outline-0"
-                        onClick={() => setOnShowPassword(!onShowPassword)}
-                        placeholder="Email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
+                        placeholder="Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
                     />
-                    {onShowPassword && (
-                        <input
-                            type="password"
-                            className="px-4 py-[10px] order-solid bg-gray-500  rounded-[15px] mb-[10px] w-[350px] text-white focus:outline-0"
-                            placeholder="Password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                        />
-                    )}
-                    <button
-                        onClick={onSignUpWithPassword}
-                        className="flex items-center justify-center p-[10px] w-[350px] bg-gray-500 text-white text-md font-bold rounded-[15px] cursor-pointer mb-[10px]"
-                    >
-                        Continue with email
-                    </button>
-                </div>
-                <div className="flex-2 flex items-center justify-center right-column bg-gray-700 ">
-                    <img
-                        src="/Group1.svg"
-                        width={800}
-                        height={450}
-                        alt="Video"
-                        className="mx-auto cursor-pointer"
-                    />
-                </div>
+                )}
+                <button
+                    onClick={onSignUpWithPassword}
+                    className="flex items-center justify-center p-[10px] w-[350px] bg-gray-500 text-white text-md font-bold rounded-[15px] cursor-pointer mb-[10px]"
+                >
+                    Continue with email
+                </button>
+            </div>
+            <div className="max-md:hidden flex-1 flex items-center justify-center  bg-gray-700 ">
+                <img
+                    src="/Group1.svg"
+                    width={800}
+                    height={450}
+                    alt="Video"
+                    className="mx-auto cursor-pointer"
+                />
             </div>
             <ToastContainer />
-        </>
+        </div>
     )
 }
